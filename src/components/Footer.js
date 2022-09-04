d40f875155936f01879a7c23116deff58a5c5cd9
Faraz Ahmed Abir <62275863+farazabir@users.noreply.github.com>
2022-09-04 16:13:44 +0600

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="d-flex justify-content-center" style={{marginTop:"25%"}}>
-      <h6  >Made By Faraz Ahmed Abir</h6>
+      <h6>Made with <span>❤️</span> by Faraz Ahmed Abir</h6>
     </div>
   )
 }
